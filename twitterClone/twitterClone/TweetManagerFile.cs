778b6abe1bcf3f1cdf1d2b2f2fd3f3731b3756b1
Deshanm123 <deshanm123@gmail.com>
2020-07-01 23:31:07 +0530

    Tweet Clone
    Added File system to Application

---
+++ twitterClone/twitterClone/TweetManagerFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+ namespace twitterClone
+{
+    public class TweetManagerFile
+    {
+     public string filename = "tweets.txt";
+
+        public string PostTweet(string tweet)
+        {
+            if (tweet.Length > 140)
+                return "Error 101: Tweeet must do not exceed 140 characters";
+
+            if (TweetCounter() >= GetTweets().Length)
+                return " Error 104:  Max Tweeets reached.";
+
+            System.IO.File.AppendAllLines(filename,new string[] {tweet});
+            //create a file if it is not exist otherwise add one tweet and close
+            return "Posted Tweet Successfully";
+        }
+        //return the number of tweets
+         public int TweetCounter()
+        {
+            return System.IO.File.ReadAllLines(filename).Length;
+            //arr.length
+        }
+
+
+        //return array of tweeets 
+         public string[] GetTweets()
+        {
+            return System.IO.File.ReadAllLines(filename);
+            //creating a array
+        }
+    }
+}
